87177c6c674d27f83c5d6be893cd4e0ce2dc131a
TeeGee-TechBro <allin1acct.com@gmail.co>
2024-07-21 21:02:12 +0100

    started a new portfolio for myself

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="teegee"
+        options={{
+          title: 'ThankGod',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/teegee.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const teegee = () => {
+  return (
+    <View>
+      <Text>teegee</Text>
+    </View>
+  )
+}
+
+export default teegee
